dd09009c3026929430bd87e4d1f6b611e5fbd895
Shubham Kumar Bind <115348960+Versus04@users.noreply.github.com>
2025-08-24 20:48:17 +0530

    subarray sum I

--- subarraysum.cpp
+++ subarraysum.cpp
@@ -3,26 +3,19 @@ using namespace std;
 
 int main()
 {
-    int n ;
-    int x;
-    cin>>n;
-    cin>>x;
-    vector<int> arr(n);
-    for(int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-    int sum = 0, count = 0;
-    unordered_map<int, int> prefixCount;
-    prefixCount[0] = 1; 
-    for(int i = 0 ;i < n;i++)
+    long long n ,target;
+    cin>>n>>target;
+    unordered_map<long long,long long>mpp;
+    long long cnt=0;
+    mpp[0]=1;
+    long long sum=0;
+    for(int i =0 ; i < n;i++)
     {
-        sum+=arr[i];
-        if(prefixCount.find(sum-x)!=prefixCount.end())
-        {
-            count+=prefixCount[sum-x];
-        }
-        prefixCount[sum]++;
+        long long x;
+        cin>>x;
+        sum+=x;
+        mpp[sum]++;
+        if(mpp.find(sum-target)!=mpp.end())cnt+=mpp[sum-target];
     }
-    cout<<count;
-
+    cout<<cnt;
 }
